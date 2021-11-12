8feeb704f063deb97d41392c4509bcd7c320ac15
TGPGamez <tobi8268@zbc.dk>
2021-11-12 12:39:17 +0100

    Commit

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,7 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using AirportLib;
+using WpfApp.ViewModel;
 
 namespace WpfApp
 {
@@ -26,6 +29,7 @@ namespace WpfApp
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new NavigationViewModel();
             InitializeAirport();
         }
 
@@ -34,25 +38,42 @@ namespace WpfApp
             Simulator = new Simulator(15, 20, 25);
             Simulator.IsAutoGenereatedReservationsEnabled = true;
             Simulator.Start();
+            new DispatcherTimer(TimeSpan.FromSeconds(0.01),
+                DispatcherPriority.Normal,
+                OnTick,
+                Dispatcher.CurrentDispatcher);
+        }
+
+        private void OnTick(object sender, EventArgs e)
+        {
+            if (Monitor.TryEnter(Simulator))
+            {
+                GL_Time_HHmm.Text = $" {Simulator.Time.DateTime.ToString("HH:mm")}";
+                GL_Time_ddMM.Text = $" {Simulator.Time.DateTime.ToString("dd-MM")}";
+                GL_Speed.Text = $"| Hastighed {Simulator.Time.Speed}x |";
+                GL_Activity.Text = $"Travlhed level {Simulator.ActivityLevel} |";
+
+                Monitor.PulseAll(Simulator);
+                Monitor.Exit(Simulator);
+            }
         }
 
         private void Sim_Speed_Click(object sender, RoutedEventArgs e)
         {
-            Button button = (Button)sender;
-            int newSpeed = (int)button.Tag;
-            Simulator.Time.Speed = newSpeed;
+            MenuItem mI = (MenuItem)sender;
+            string newSpeed = mI.Tag.ToString();
+            Simulator.Time.Speed = int.Parse(newSpeed);
         }
 
         private void Sim_Activity_Click(object sender, RoutedEventArgs e)
         {
-            Button button = (Button)sender;
-            int newActivity = (int)button.Tag;
-            Simulator.ActivityLevel = newActivity;
+            MenuItem mI = (MenuItem)sender;
+            string newActivity = mI.Tag.ToString();
+            Simulator.ActivityLevel = int.Parse(newActivity);
         }
 
-        private void Window_Closed(object sender, EventArgs e)
-        {
-            Simulator.Stop();
-        }
+        private void Window_Closed(object sender, EventArgs e) => Simulator.Stop();
+
+        private void Exit_Click(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
     }
 }
---
+++ WpfApp/ViewModel/NavigationViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp.ViewModel
+{
+    class NavigationViewModel : INotifyPropertyChanged
+    {
+        public ICommand OverViewCommand { get; set; }
+        public ICommand LogViewCommand { get; set; }
+
+        //public ICommand DeptCommand { get; set; }
+
+        private object selectedViewModel;
+
+        public object SelectedViewModel
+
+        {
+
+            get { return selectedViewModel; }
+
+            set { selectedViewModel = value; OnPropertyChanged("SelectedViewModel"); }
+
+        }
+
+
+
+        public NavigationViewModel()
+
+        {
+
+            OverViewCommand = new BaseCommand(OpenOverView);
+
+            LogViewCommand = new BaseCommand(OpenLogView);
+
+        }
+
+        private void OpenOverView(object obj)
+
+        {
+
+            SelectedViewModel = new OverviewViewModel();
+
+        }
+
+        private void OpenLogView(object obj)
+
+        {
+
+            SelectedViewModel = new ConsoleViewModel();
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propName)
+
+        {
+
+            if (PropertyChanged != null)
+
+            {
+
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+
+            }
+
+        }
+    }
+}
